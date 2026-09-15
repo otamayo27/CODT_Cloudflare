cad6c3597eab216d978999d21e9b7dcdf05c8371
otamayo27 <otamayo27@gmail.com>
2026-09-14 18:26:32 -0600

    Agregar catálogo de empresas y duplas

---
+++ src/personnel.js
@@ -0,0 +1,431 @@
+// Catálogo operativo proporcionado por el usuario. No contiene nombres personales ni datos de contacto.
+export const personnelByResponsible = {
+  "BMORAL": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "DLMEND": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "HGARCI": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "FMESTI": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "EVALLE": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "RCANTA": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "JMENDO": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "RRIVAS": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "JOSORI": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "RHERNA": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "WLOPEZ": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "MHERRE": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "RSEVIL": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "FLOPEZ": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "JZELAD": {
+    "empresa": "CORDEROS",
+    "funcion": "DUP. TECNICA"
+  },
+  "JRHERN": {
+    "empresa": "CORDEROS",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "IAZUCE": {
+    "empresa": "DELSUR",
+    "funcion": "Administrador"
+  },
+  "RCOTO": {
+    "empresa": "DELSUR",
+    "funcion": "Administrador"
+  },
+  "MARBAI": {
+    "empresa": "DELSUR",
+    "funcion": "Coordinador"
+  },
+  "MCASTI": {
+    "empresa": "DELSUR",
+    "funcion": "INST. MEDICIONES"
+  },
+  "JGOMEZ": {
+    "empresa": "DELSUR",
+    "funcion": "INST. MEDICIONES"
+  },
+  "CHUEZO": {
+    "empresa": "DELSUR",
+    "funcion": "INST. MEDICIONES"
+  },
+  "AFERRE": {
+    "empresa": "DELSUR",
+    "funcion": "JEFE. DPTO."
+  },
+  "LAPARICI": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "JHERRE": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "MCUBIA": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "NHERNA": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "SCASTR": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "RSANCH": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "PSORIANO": {
+    "empresa": "DELSUR",
+    "funcion": "SUPERVISOR"
+  },
+  "CCHAVE": {
+    "empresa": "EBC",
+    "funcion": "DUP. ESPECIAL"
+  },
+  "OGALDA": {
+    "empresa": "EBC",
+    "funcion": "DUP. ESPECIAL"
+  },
+  "JALFAR": {
+    "empresa": "EBC",
+    "funcion": "DUP. TECNICA"
+  },
+  "JCORTE": {
+    "empresa": "EBC",
+    "funcion": "DUP. TECNICA"
+  },
+  "MGONZA": {
+    "empresa": "EBC",
+    "funcion": "DUP. TECNICA"
+  },
+  "KRAMIR": {
+    "empresa": "EBC",
+    "funcion": "DUP. TECNICA"
+  },
+  "JCORAD": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "MARGUE": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "CHERNA": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "ASIERR": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "HVASQU": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "EACRUZ": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "JVASQU": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "LDIAZ": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "SCAMPO": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "SYANES": {
+    "empresa": "ESEM",
+    "funcion": "DUP. TECNICA"
+  },
+  "BESCAL": {
+    "empresa": "ESEM",
+    "funcion": "V. RECLAMOS"
+  },
+  "JSERRA": {
+    "empresa": "ESEM",
+    "funcion": "V. RECLAMOS"
+  },
+  "MEESCA": {
+    "empresa": "ESEM",
+    "funcion": "V. RECLAMOS"
+  },
+  "MAGRANDE": {
+    "empresa": "INNOVA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "RCERON": {
+    "empresa": "INNOVA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "DGUERR": {
+    "empresa": "INNOVA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "WORTIZ": {
+    "empresa": "INNOVA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "DARIVA": {
+    "empresa": "INNOVA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "CAMAYA": {
+    "empresa": "L. ENERGY",
+    "funcion": "DUP. TECNICA"
+  },
+  "KAPERD": {
+    "empresa": "L. ENERGY",
+    "funcion": "DUP. TECNICA"
+  },
+  "SDIAZ": {
+    "empresa": "L. ENERGY",
+    "funcion": "DUP. TECNICA"
+  },
+  "JCRODR": {
+    "empresa": "L. ENERGY",
+    "funcion": "DUP. TECNICA"
+  },
+  "SAGARC": {
+    "empresa": "ODES",
+    "funcion": "MOTORIZADO"
+  },
+  "BUMORA": {
+    "empresa": "ODES",
+    "funcion": "MOTORIZADO"
+  },
+  "OSALIN": {
+    "empresa": "ODES",
+    "funcion": "MOTORIZADO"
+  },
+  "CEHERN": {
+    "empresa": "ODES",
+    "funcion": "MOTORIZADO"
+  },
+  "JVMATA": {
+    "empresa": "SEPER",
+    "funcion": "SUPERVISOR"
+  },
+  "MQUEZA": {
+    "empresa": "SERVINTA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "JPORTI": {
+    "empresa": "SERVINTA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "LPEREZ": {
+    "empresa": "SERVINTA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "JROJAS": {
+    "empresa": "SERVINTA",
+    "funcion": "V. FACTIBILIDADES"
+  },
+  "HCORTE": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "IFLORES": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JRODRIGU": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JBERNAL": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JCORDE": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "WALBERTO": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JJRAMIR": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "MRODIN": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "MRAMOS": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "OPALAC": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "AAPERE": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "CRODRI": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "CAVALO": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JDURAN": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "VJEORG": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "SHERNA": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "MCHENR": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "CCRESP": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "SGARCI": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JPARED": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "EMORAN": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "DSIBRI": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "DBENAV": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "OMORAL": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "MCORNE": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "SOLMEDO": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "RRUBIO": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "ODELGA": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "RCONTRER": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "GJGARCIA": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JDIMAS": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "SRIVERA": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JREYES": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "HGUTIERR": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "MRVASQUE": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "DFMUNOZ": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "JDIRAHET": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "CBARDA": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "GCISNA": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  },
+  "CELOPEZ": {
+    "empresa": "DESCONOCIDO",
+    "funcion": "DESCONOCIDO"
+  }
+}
